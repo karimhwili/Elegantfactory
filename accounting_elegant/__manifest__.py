4876e6608a871717fa9f4cc145074286b815182c
Your Name <you@example.com>
2021-03-31 03:57:05 +0200

    [MOD]assets and accounting dashboard

--- accounting_elegant/__manifest__.py
+++ accounting_elegant/__manifest__.py
@@ -9,6 +9,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'groups/groups.xml',
         'views/account_assets.xml',
         'views/journal_entry.xml',
--- accounting_elegant/models/account_assets.py
+++ accounting_elegant/models/account_assets.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+
 
 class AccountAssetsInherit(models.Model):
     _inherit = 'account.asset'
@@ -8,6 +9,19 @@ class AccountAssetsInherit(models.Model):
     tracking = fields.One2many('assets.tracking','asset_id',"Tracking")
     original_value = fields.Float(compute="_get_original_value", store=True)
     salvage_value = fields.Float(compute="_get_original_value", store=True,readonly=False)
+    asset_types = fields.Many2one('asset.type',"Asset Type")
+    seq = fields.Char(string="Sequence",  required=True, copy=False, readonly=True,
+                           index=True, default='New')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('seq', _('New')) == _('New'):
+            vals['seq'] = self.env['ir.sequence'].next_by_code(
+                'account.asset.seq') or _('New')
+
+        result = super(AccountAssetsInherit, self).create(vals)
+        return result
+
 
 
     @api.depends('tracking.value', 'tracking.depreciable_value')
@@ -30,6 +44,13 @@ class TrackingAssets(models.Model):
     reference = fields.Char("Reference")
     value = fields.Float("Value")
     depreciable_value = fields.Float("Depreciable Value")
+
+class AssetsType(models.Model):
+    _name = 'asset.type'
+    _description = 'Assets Type'
+
+    name = fields.Char("Type")
+
 
 
 
