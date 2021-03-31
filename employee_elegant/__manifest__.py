d67d5f9db9bcb487f662fda41a8a5373498c944a
Your Name <you@example.com>
2021-03-31 20:34:57 +0200

    [ADD] Employee customization

---
+++ employee_elegant/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employee Elegant",
+
+    'author': "M.Shorbagy (Sahara)",
+
+    'depends': ['base','hr','account_asset'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'views/employee.xml',
+
+    ],
+
+}
