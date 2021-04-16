658710cd7c3e7f9889c2912460b5695784b7351c
Your Name <you@example.com>
2021-04-16 03:38:50 +0200

    [ADD] Elegant Customization

---
+++ stock_location_limit_product/__manifest__.py
@@ -0,0 +1,13 @@
+
+
+{
+    'name': 'Stock Location Limit Product',
+    'depends': [
+        'base','stock',
+    ],
+    'data': [
+
+        'views/stock_location_view.xml',
+    ],
+    'installable': True,
+}
