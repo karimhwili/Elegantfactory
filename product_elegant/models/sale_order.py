b49c53144fd2f75a207db2b15d30225969073b86
Your Name <you@example.com>
2021-04-22 06:29:05 +0200

    [MOD]Product limitation

--- product_elegant/models/sale_order.py
+++ product_elegant/models/sale_order.py
@@ -7,11 +7,24 @@ class SaleOrderInherit(models.Model):
 
     def action_confirm(self):
         credit_product = 0
-        for order in self.search([('state','in',('draft','sale'))]):
+        inv_credit = 0
+
+        inv_rec = self.env['account.move'].search([
+            ('partner_id', '=', self.partner_id.id),
+            ('amount_residual','!=',0.0)])
+
+        for inv in inv_rec:
+            for line in inv.invoice_line_ids:
+                for partner in line.product_id.product_limits:
+                    if inv.partner_id == partner.customer_id:
+                        inv_credit += line.price_subtotal
+
+        for order in self:
             for line in order.order_line:
                 for partner in line.product_id.product_limits:
                     if order.partner_id == partner.customer_id:
                         credit_product += line.price_subtotal
-                        if credit_product > partner.limit:
-                            raise ValidationError(_('You can not set quantity for product %s more than %s',line.product_id.name,partner.limit))
+                    total_sales = inv_credit + credit_product
+                    if total_sales > partner.limit:
+                        raise ValidationError(_('You can not set amount for product %s more than %s',line.product_id.name,partner.limit))
         super(SaleOrderInherit, self).action_confirm()
