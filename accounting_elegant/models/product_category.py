2afa95a8c2a441a7e63b6c8d1be076a651fc17b5
hassan <hassan8ghannoum@gmail.com>
2021-05-09 01:02:37 +0300

    fix

--- accounting_elegant/models/product_category.py
+++ accounting_elegant/models/product_category.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class ProductCategory(models.Model):
     _inherit = 'product.category'
     property_scrap_account_id = fields.Many2one('account.account', 'Scrap Account',
-                                                domain="[('internal_group','=','expense')]", company_dependent=True)
+                                                domain="[('internal_group', '=', 'expense')]", company_dependent=True)
 
     income_add_stock_account_id = fields.Many2one(
-        'account.account', string='Add Account', domain="[('internal_group','=','expense'))]")
+        'account.account', string='Add Account', domain="[('internal_group','=','expense')]", company_dependent=True)
