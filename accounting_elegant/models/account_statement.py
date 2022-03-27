04fd7e7978dbe0d2f050b73e9d17471ba1c75d4d
Your Name <you@example.com>
2022-03-28 01:14:23 +0200

    [MOD]

--- accounting_elegant/models/account_statement.py
+++ accounting_elegant/models/account_statement.py
@@ -8,11 +8,11 @@ class ResUsers(models.Model):
     computed_balance = fields.Boolean("Make statement with negative value")
 
 
-class AccountStatement(models.Model):
-    _inherit = 'account.bank.statement'
-
-    @api.constrains('balance_end')
-    def _check_balance_end(self):
-        for rec in self:
-            if rec.balance_end < 0 and rec.user_id.computed_balance == False:
-                raise ValidationError(_('The Computed Balance should be Positive.'))
+# class AccountStatement(models.Model):
+#     _inherit = 'account.bank.statement'
+#
+#     @api.constrains('balance_end')
+#     def _check_balance_end(self):
+#         for rec in self:
+#             if rec.balance_end < 0 and rec.user_id.computed_balance == False:
+#                 raise ValidationError(_('The Computed Balance should be Positive.'))
