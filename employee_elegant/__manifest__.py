82fd690e28b3e9795f4dfa6ac6d18e96e398c987
Your Name <you@example.com>
2023-03-08 01:02:20 +0200

    mod sales team

--- employee_elegant/__manifest__.py
+++ employee_elegant/__manifest__.py
@@ -4,17 +4,19 @@
 
     'author': "M.Shorbagy (Sahara)",
 
-    'depends': ['base','hr','account_asset','hr_contract','hr_holidays','hr_payroll'],
+    'depends': ['base','hr','account_asset','hr_contract','hr_holidays','hr_payroll','sales_team'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'data/allocation_schdule_data.xml',
         'data/sequence.xml',
         'views/employee.xml',
         'views/hr_township.xml',
         'views/leave_type.xml',
-        'views/payslip_report.xml'
+        'views/payslip_report.xml',
+        'views/sales_team.xml'
 
     ],
 
